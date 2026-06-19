b3ac6160c531d8618cbd33fcb6c6fd8f60b05e2e
cibelius <cihan.m.bebek@gmail.com>
2026-06-19 12:25:44 +0300

    fix

--- src/events/birth/index.ts
+++ src/events/birth/index.ts
@@ -384,7 +384,7 @@ export const birthEvent = defineConfig({
         register: {
           id: 'event.birth.action.edit.register.copy',
           defaultMessage:
-            'You are about to register this birth event with your edits. Registering this event will create an official civil registration record.',
+            'You are about to register this birth event with your edits. Please ensure all details are correct before proceeding.<br></br><br></br><strong>WARNING!</strong>: By continuing, you confirm that you have reviewed the record alongside supporting documentation. The record will proceed to be <strong>legally registered</strong> via the outbox. Further amends after registration can only be made via a legal correction process.',
           description: 'Confirmation text for the register with edits action'
         }
       }
@@ -890,7 +890,7 @@ export const birthEvent = defineConfig({
         id: 'event.birth.action.register.supportingCopy',
         description: 'Confirmation text for the register action',
         defaultMessage:
-          'Registering this birth event will create an official civil registration record. Please ensure all details are correct before proceeding.'
+          "Registering this birth event will create an official civil registration record. Please ensure all details are correct before proceeding.<br></br><br></br><strong>WARNING!</strong>: By clicking 'Register', you confirm that you have reviewed the record alongside supporting documentation in the Record tab. The record will proceed to be <strong>legally registered</strong> via the outbox. Further amends after registration can only be made via a legal correction process."
       },
       flags: [
         { id: 'validated', operation: 'remove' },
